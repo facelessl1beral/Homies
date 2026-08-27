b818feb8035cdf3a35817ea320488313e59524e6
Sserunkuma Barry David <bkick7511@gmail.com>
2026-08-27 06:04:21 +0300

    chore: fix OpenSSL build failure, track complete production build

---
+++ client/build/precache-manifest.5db212b983b2dd35cc37b61d623c9ef9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55263de89935e0a774243f00597df824",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f8fc2bdbf74a035c684e",
+    "url": "/static/css/2.5e8174b9.chunk.css"
+  },
+  {
+    "revision": "e7f826d433211f7625f5",
+    "url": "/static/css/main.b41e318c.chunk.css"
+  },
+  {
+    "revision": "f8fc2bdbf74a035c684e",
+    "url": "/static/js/2.af2801e5.chunk.js"
+  },
+  {
+    "revision": "e7f826d433211f7625f5",
+    "url": "/static/js/main.8b31a184.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a7dcb2229e46a9638586bf332ddcbcff",
+    "url": "/static/media/hostel.a7dcb222.jpeg"
+  },
+  {
+    "revision": "bb338a8c77c54a9a7762d9a907a70590",
+    "url": "/static/media/rooms.bb338a8c.png"
+  }
+]);
